59cb16e755b46f854549caaaabd01ff8aad682c6
Ntsikelelo <mbekwa92@gmail.com>
2018-08-11 13:26:19 +0200

    init

---
+++ app/src/main/java/com/example/waterusagediary/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.waterusagediary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openCalculator(View view){
+        Intent intent = new Intent(this, Calculator.class);
+        startActivity(intent);
+
+    }
+}
